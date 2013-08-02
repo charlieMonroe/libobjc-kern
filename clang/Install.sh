e28a464187813e7ffd3600363147ac35c0de9fb4
Krystof Vasa <kvasa@me.com>
2013-08-02 13:18:43 +0100

    Adding stuff from LLVM/Clang

---
+++ clang/Install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cp ~/libobjc-kern/clang/ObjCRuntime.h ~/llvm/tools/clang/include/clang/Basic/ObjCRuntime.h
+cp ~/libobjc-kern/clang/Options.td ~/llvm/tools/clang/include/clang/Driver/Options.td
+
+cp ~/libobjc-kern/clang/ASTContext.cpp ~/llvm/tools/clang/lib/AST/ASTContext.cpp
+cp ~/libobjc-kern/clang/ObjCRuntime.cpp ~/llvm/tools/clang/lib/Basic/ObjCRuntime.cpp
+cp ~/libobjc-kern/clang/CGException.cpp ~/llvm/tools/clang/lib/CodeGen/CGException.cpp
+cp ~/libobjc-kern/clang/CGObjCGNU.cpp ~/llvm/tools/clang/lib/CodeGen/CGObjCGNU.cpp
+cp ~/libobjc-kern/clang/CGObjCMac.cpp ~/llvm/tools/clang/lib/CodeGen/CGObjCMac.cpp
+cp ~/libobjc-kern/clang/CodeGenModule.cpp ~/llvm/tools/clang/lib/CodeGen/CodeGenModule.cpp
+cp ~/libobjc-kern/clang/Tools.cpp ~/llvm/tools/clang/lib/Driver/Tools.cpp
+
