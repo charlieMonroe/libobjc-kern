eb3008ecaa265a5714f38b4a3d22278a14057e60
Krystof Vasa <kvasa@me.com>
2013-08-14 14:21:55 +0100

    elf_file_t

--- kernel_module.c
+++ kernel_module.c
@@ -21,17 +21,67 @@ SET_DECLARE(objc_module_list_set, struct objc_loader_module);
 
 // extern void run_tests(void);
 
+typedef struct elf_file {
+	struct linker_file lf;		/* Common fields */
+	int		preloaded;	/* Was file pre-loaded */
+	caddr_t		address;	/* Relocation address */
+#ifdef SPARSE_MAPPING
+	vm_object_t	object;		/* VM object to hold file pages */
+#endif
+	Elf_Dyn		*dynamic;	/* Symbol table etc. */
+	Elf_Hashelt	nbuckets;	/* DT_HASH info */
+	Elf_Hashelt	nchains;
+	const Elf_Hashelt *buckets;
+	const Elf_Hashelt *chains;
+	caddr_t		hash;
+	caddr_t		strtab;		/* DT_STRTAB */
+	int		strsz;		/* DT_STRSZ */
+	const Elf_Sym	*symtab;		/* DT_SYMTAB */
+	Elf_Addr	*got;		/* DT_PLTGOT */
+	const Elf_Rel	*pltrel;	/* DT_JMPREL */
+	int		pltrelsize;	/* DT_PLTRELSZ */
+	const Elf_Rela	*pltrela;	/* DT_JMPREL */
+	int		pltrelasize;	/* DT_PLTRELSZ */
+	const Elf_Rel	*rel;		/* DT_REL */
+	int		relsize;	/* DT_RELSZ */
+	const Elf_Rela	*rela;		/* DT_RELA */
+	int		relasize;	/* DT_RELASZ */
+	caddr_t		modptr;
+	const Elf_Sym	*ddbsymtab;	/* The symbol table we are using */
+	long		ddbsymcnt;	/* Number of symbols */
+	caddr_t		ddbstrtab;	/* String table */
+	long		ddbstrcnt;	/* number of bytes in string table */
+	caddr_t		symbase;	/* malloc'ed symbold base */
+	caddr_t		strbase;	/* malloc'ed string base */
+	caddr_t		ctftab;		/* CTF table */
+	long		ctfcnt;		/* number of bytes in CTF table */
+	caddr_t		ctfoff;		/* CTF offset table */
+	caddr_t		typoff;		/* Type offset table */
+	long		typlen;		/* Number of type entries. */
+	Elf_Addr	pcpu_start;	/* Pre-relocation pcpu set start. */
+	Elf_Addr	pcpu_stop;	/* Pre-relocation pcpu set stop. */
+	Elf_Addr	pcpu_base;	/* Relocated pcpu set address. */
+#ifdef VIMAGE
+	Elf_Addr	vnet_start;	/* Pre-relocation vnet set start. */
+	Elf_Addr	vnet_stop;	/* Pre-relocation vnet set stop. */
+	Elf_Addr	vnet_base;	/* Relocated vnet set address. */
+#endif
+#ifdef GDB
+	struct link_map	gdb;		/* hooks for gdb */
+#endif
+} *elf_file_t;
+
 
 static void get_elf(struct module *module){
-	linker_file_t file = module_file(module);
-	Elf_Phdr *phdr = (Elf_Phdr *)file->address;
-	objc_log("Found ELF program header %p\n", phdr);
-	if (phdr == NULL){
-		return;
-	}
-
-
+	elf_file_t file = (elf_file_t)module_file(module);
 	
+	objc_log("ELF file dump (%p):\n", file);
+	objc_log("\preloaded: \t\t%i\n", file->preloaded);
+	objc_log("\address: \t\t%p\n", file->address);
+	objc_log("\ddbsymtab: \t\t0x%lx\n", file->ddbsymtab);
+
+
+/*
 	objc_log("PHDR dump:\n");
 	objc_log("\tp_type: \t\t%u\n", phdr->p_type);
 	objc_log("\tp_flags: \t\t%u\n", phdr->p_flags);
@@ -41,7 +91,7 @@ static void get_elf(struct module *module){
 	objc_log("\tp_filesz: \t\t%lu\n", phdr->p_filesz);
 	objc_log("\tp_memsz: \t\t%lu\n", phdr->p_memsz);
 	objc_log("\tp_align: \t\t%lu\n", phdr->p_align);
-	
+*/	
 }
 
 static int event_handler(struct module *module, int event, void *arg) {
