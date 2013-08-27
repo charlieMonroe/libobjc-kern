87d9bc3cc041a65fc75de67ae2e87be639e25c28
charlieMonroe <kvasa@me.com>
2013-08-27 12:44:04 +0100

    The compiler requires NSString which we don't currently have...

--- test/string-test.m
+++ test/string-test.m
@@ -5,7 +5,7 @@
 
 void string_test(void);
 void string_test(void){
-	_KKConstString *string = @"Hello";
+	id string = @"Hello";
 	
 	objc_assert([string length] == 5, "Wrong string length!\n");
 	objc_assert(objc_strings_equal("Hello", [string cString]),
