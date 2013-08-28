5c75fe9d5a3d722ab2f8db3ec26cd8e6cae1535a
charlieMonroe <kvasa@me.com>
2013-08-28 10:01:02 +0100

    Moving to a folder with capital F

---
+++ Foundation__/NSMutableDictionary.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableDictionary.h
+//  libkernobjc_xcode
+//
+//  Created by Charlie Monroe on 8/28/13.
+//  Copyright (c) 2013 Krystof Vasa. All rights reserved.
+//
+
+#import "NSDictionary.h"
+
+@interface NSMutableDictionary : NSDictionary
+
+@end
---
+++ Foundation__/NSMutableDictionary.m
@@ -0,0 +1,13 @@
+//
+//  NSMutableDictionary.m
+//  libkernobjc_xcode
+//
+//  Created by Charlie Monroe on 8/28/13.
+//  Copyright (c) 2013 Krystof Vasa. All rights reserved.
+//
+
+#import "NSMutableDictionary.h"
+
+@implementation NSMutableDictionary
+
+@end
