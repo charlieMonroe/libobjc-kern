1cd1203f366afdf8fbd679d9a2eef16950792b07
charlieMonroe <kvasa@me.com>
2013-09-02 09:08:35 +0100

    Var name conflict.

--- test/unload-test/messee/test.m
+++ test/unload-test/messee/test.m
@@ -13,17 +13,19 @@ void run_test(void);
 void run_test(void){
 	MyClass *obj = [[MyClass alloc] init];
 	
-	BOOL exception = NO;
+	BOOL exceptionWasCaught = NO;
 	@try {
 		[obj sayHello];
 	}
 	@catch (id exception) {
-		exception = YES;
+		exceptionWasCaught = YES;
 		objc_log("Caught exception of class %s\n",
 			 object_getClassName(exception));
 	}
 	
-	objc_assert(exception, "Exception was not thrown!\n");
+	objc_assert(exceptionWasCaught, "Exception was not thrown!\n");
 	
 	[obj release];
+	
+	objc_log("Passed unload test.\n");
 }
