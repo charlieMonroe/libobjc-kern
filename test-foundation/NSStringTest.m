958dcd36c6fcfdf56e4f1c37d0df68a00d6970dc
charlieMonroe <kvasa@me.com>
2013-09-04 13:09:51 +0100

    NSlog test

--- test-foundation/NSStringTest.m
+++ test-foundation/NSStringTest.m
@@ -9,6 +9,9 @@ void run_string_test(void){
 		const char *cString = [constString UTF8String];
 		objc_assert(objc_strings_equal(cString, "Hello World"), "Wrong value\n");
 		
+		
+		
+		NSLog(@"Hello %02i %s!", 123, world);
 	}
 }
 
