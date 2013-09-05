e0cdf3c0aa83ff5381c3d9eda86fd88ad3c898a0
charlieMonroe <kvasa@me.com>
2013-09-05 13:59:00 +0100

    Removing NSObject protocol

--- Foundation/NSObject.h
+++ Foundation/NSObject.h
@@ -2,21 +2,13 @@
 #import "../kernobjc/KKObjects.h"
 #import "NSTypes.h"
 
-@protocol NSObject
-
--(id)autorelease;
--(void)release;
--(id)retain;
-
-@end
-
 @class NSArray, NSMethodSignature;
 
 /*
  * Based on KKObject, NSObject implements what KKObject doesn't. KKObject is
  * meant to be a super-light version of NSObject.
  */
-@interface NSObject : KKObject <NSObject>
+@interface NSObject : KKObject
 
 +(id)allocWithZone:(NSZone*)zone;
 +(id)className;
