39b4e6c28c6692b222cd647f92a71479c26b684e
charlieMonroe <kvasa@me.com>
2013-09-12 10:52:02 +0100

    Load

--- LanguageKit/CGObjCGNU.mm
+++ LanguageKit/CGObjCGNU.mm
@@ -691,7 +691,7 @@ llvm::Value *CGObjCGNU::callIMP(
 	llvm::Constant *Two = llvm::ConstantInt::get(IntTy, 2);
 	llvm::Value *ExcGEPIndexes[] = { Zero, Zero, Two };
 	llvm::Value *ExceptionResult = ExceptionBuilder.CreateGEP(ExceptionData, ExcGEPIndexes, "exc_obj");
-	ExceptionBuilder.CreateStore(ExceptionResult, ret);
+	ExceptionBuilder.CreateStore(ExceptionBuilder.CreateLoad(ExceptionResult), ret);
 	
 	if (ReturnTy != Type::getVoidTy(Context))
 	{
